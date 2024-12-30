3925b713224e8b08f83733f36af8d0b76815dcc6
alfeifal <ayoub.afak.aaa@gmail.com>
2024-12-30 22:06:19 +0100

    add rarity command

--- ballsdex/packages/balls/cog.py
+++ ballsdex/packages/balls/cog.py
@@ -487,6 +487,61 @@ class Balls(commands.GroupCog, group_name=settings.players_group_cog_name):
                 ephemeral=True,
             )
 
+    @app_commands.command()
+    @app_commands.checks.has_any_role(*settings.root_role_ids)
+    async def rarity(self, interaction: discord.Interaction["BallsDexBot"], chunked: bool = True):
+        # DO NOT CHANGE THE CREDITS TO THE AUTHOR HERE!
+        """
+        Show the ACTUAL rarities of the dex - made by GamingadlerHD
+        """
+        # Filter enabled collectibles
+        enabled_collectibles = [x for x in balls.values() if x.enabled]
+
+        if not enabled_collectibles:
+            await interaction.response.send_message(
+                f"There are no collectibles registered in {settings.bot_name} yet.",
+                ephemeral=True,
+            )
+            return
+
+        # Sort collectibles by rarity in ascending order
+        sorted_collectibles = sorted(enabled_collectibles, key=lambda x: x.rarity)
+
+        entries = []
+
+        for collectible in sorted_collectibles:
+            name = f"{collectible.country}"
+            emoji = self.bot.get_emoji(collectible.emoji_id)
+
+            if emoji:
+                emote = str(emoji)
+            else:
+                emote = "N/A"
+            # if you want the Rarity to only show full numbers like 1 or 12 use the code part here:
+            # rarity = int(collectible.rarity)
+            # otherwise you want to display numbers like 1.5, 5.3, 76.9 use the normal part.
+            rarity = collectible.rarity
+
+            entry = (name, f"{emote} Rarity: {rarity}")
+            entries.append(entry)
+        # This is the number of countryballs who are displayed at one page,
+        # you can change this, but keep in mind: discord has an embed size limit.
+        per_page = 5
+
+        source = FieldPageSource(entries, per_page=per_page, inline=False, clear_description=False)
+        source.embed.description = (
+            f"__**{settings.bot_name} rarity**__"
+        )
+        source.embed.colour = discord.Colour.blurple()
+        source.embed.set_author(
+            name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url
+        )
+
+        pages = Pages(source=source, interaction=interaction, compact=True)
+        await pages.start(
+            ephemeral=True,
+        )
+
     @app_commands.command(extras={"trade": TradeCommandType.PICK})
     async def give(
         self,
